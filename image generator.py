c3c1f49cba2ade1e6e6ba9e2507f17130018e006
Stromy08 <125371422+Stromy08@users.noreply.github.com>
2024-06-19 19:21:43 +0200

    Revert "basic avatar fetching (currently broken)"
    This reverts commit ba2e5b11d558fde6005f128c645ff331c13e83cd.

--- image generator.py
+++ image generator.py
@@ -1,5 +1,4 @@
-from PIL import Image, ImageDraw, ImageFont
-
+from PIL import Image
 
 pfp_1 = Image.open('pfp_1.png')
 pfp_2 = Image.open('pfp_2.png')
@@ -11,8 +10,8 @@ pfp_1_img = pfp_1.convert("RGBA")
 pfp_2_img = pfp_2.convert("RGBA")
 
 
-pos1 = (0, 250)
-pos2 = (1000, 250)
+pos1 = (0, 0)
+pos2 = (200, 0)
 
 
 bg_img.paste(pfp_1_img, pos1, pfp_1_img)
@@ -20,14 +19,3 @@ bg_img.paste(pfp_2_img, pos2, pfp_2_img)
 
 
 bg_img.save('result_image.png')
-
-image = Image.open("result_image.png")
-draw = ImageDraw.Draw(image)
-text = "Hello, World!"
-font_size = 360  # Specify the font size in points
-font = ImageFont.load_default()  # Default font
-# If you have a TrueType font file (.ttf), you can load it like this:
-# font = ImageFont.truetype("arial.ttf", size=36)
-position = (750, 250)  # Top-left corner
-draw.text(position, text, fill="white", font=font,)
-image.save('image_with_text.png')
